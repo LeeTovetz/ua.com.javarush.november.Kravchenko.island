6033a1d22ee25a1f0031c804148cd993ebde6fd3
Serge Kravchenko <55243765+LeeTovetz@users.noreply.github.com>
2022-12-28 00:10:17 +0200

    wrong file
    this file must be 
    in a different folder

--- src/main/java/ua/com/javarush/november/Kravchenko/island/Runner.java
+++ src/main/java/ua/com/javarush/november/Kravchenko/island/Runner.java
@@ -1,3 +1,17 @@
-// I have not yet figured 
-// out how to delete this folder)))
+package ua.com.javarush.november.kravchenko.island;
+
+import ua.com.javarush.november.kravchenko.island.system.*;
+
+/* You can set the size of the island
+   in the file "resources/island.properties".
+   Now it is 2*2 cells. And run the application))
+ */
+
+public class Runner {
+    public static void main(String[] args) {
+        Run run = new Run();
+        Run.RunApp();
+    }
+}
+
 
